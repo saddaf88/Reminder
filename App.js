9afbb8443d0aab7b25357db6cc3905ea608c9e4f
Saddaf Afrin Khan <saddaf88@gmail.com>
2021-04-21 14:12:49 +0200

    Add - A notification is scheduled on button clicked

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
-import { clickNotification } from './src/notification'
+import { clickNotification, showScheduledNotification } from './src/notification'
 
 class App extends Component {
 	constructor(props) {
@@ -13,12 +13,22 @@ class App extends Component {
 		clickNotification("980022", "test", "My 1st notification")
 	}
 
+	clickForScheduleNotification = () => {
+		console.log("Scheduling a notification");
+		showScheduledNotification('980022', 'sche notify', 'This is a schedule notififcation');
+	}
+
 	render() {
 		return (
 			<View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
 				<Button
 					title="Click for notification"
 					onPress={this.clickForNotification}
+				/>
+
+				<Button
+					title="Click for scheduling a notification"
+					onPress={this.clickForScheduleNotification}
 				/>
 			</View>
 		);
--- src/notification.js
+++ src/notification.js
@@ -8,5 +8,16 @@ const clickNotification = (channel, title, msg) => {
 		message: msg
 	})
 }
-// }
-export { clickNotification }
+
+const showScheduledNotification = (channel, title, msg) => {
+	PushNotification.localNotificationSchedule({
+		channelId: channel,
+		title: title,
+		message: msg, // (required)
+		date: new Date(Date.now() + 30 * 1000), // in 60 secs
+		allowWhileIdle: true, // (optional) set notification to work while on doze, default: false
+	});
+
+}
+
+export { clickNotification, showScheduledNotification }
